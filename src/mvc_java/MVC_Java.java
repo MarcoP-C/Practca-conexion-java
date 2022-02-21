7208514b25f1d864763bcf4f2e861c4db62f8f30
Fatima Itzel <fatimaitzelmh890@micorreo.upp.edu.mx>
2022-02-21 15:24:14 -0600

    Actualizacion modificaciones

---
+++ src/mvc_java/MVC_Java.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc_java;
+
+import Controlador.ControladorCliente;
+import Controlador.ControladorMDB;
+import Controlador.ControladorSeguimientoCliente;
+import Modelo.MDB;
+import Vista.FrmCliente;
+import Vista.FrmSeguimientoCliente;
+import Vista.FrmVista;
+
+/**
+ *
+ * @author Stevia
+ */
+public class MVC_Java {
+// La parte del cliente fue agregada por Marco Palacios
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //FrmCliente
+        MDB objModelo=new MDB();
+        FrmVista objVista=new FrmVista();
+        FrmCliente objCliente=new FrmCliente();
+        
+        ControladorMDB objController;
+        ControladorCliente objController1;
+        
+        objController = new ControladorMDB(objVista,objModelo);
+        objController1 = new ControladorCliente(objCliente,objModelo);
+        objController.iniciar();
+        objController1.iniciar();
+        objVista.setVisible(true);
+        objCliente.setVisible(true);
+        //FrmSeguimientoCliente
+        MDB objModeloSc=new MDB();
+        FrmSeguimientoCliente objVistaSC=new FrmSeguimientoCliente();
+        ControladorSeguimientoCliente objControllerSC;
+        
+        objControllerSC = new ControladorSeguimientoCliente(objVistaSC,objModeloSc);
+        objControllerSC.iniciar();
+        objVistaSC.setVisible(true); 
+    }
+}
