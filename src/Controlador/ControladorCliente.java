6bf647cff63339492755ea532b6644684ce82d25
juca <martinezlabrajc@micorreo.upp.edu.mx>
2022-02-21 10:21:17 -0600

    practica en clase <3

--- src/Controlador/ControladorCliente.java
+++ src/Controlador/ControladorCliente.java
@@ -33,7 +33,7 @@ public void actionPerformed(ActionEvent e) {
 
         if (e.getSource()==_view.cmdAlta){ //Si el botón presionado fue el de alta
          // System.out.print("paso");
-        leyenda=_model.registrarAlta("clientes"," null, ' " +_view.txtCedula.getText()+" ',' " +_view.txtLugarExp.getText() +" ', "+_view.txtNombreCom.getText()+" ', "+_view.txtProfesion.getText()+" ', "+_view.txtDireccion.getText()+" ', "+_view.txtCiudad.getText()+" ', "+_view.txtDepartamento.getText() );//**************
+        leyenda=_model.registrarAlta("clientes"," null, ' " +_view.txtCedula.getText()+" ',' " +_view.txtLugarExp.getText() +" ', "+_view.txtNombreCom.getText()+" ', "+_view.txtProfesion.getText()+" ', "+_view.txtDireccion.getText()+" ', "+_view.txtCiudad.getText()+" ', "+_view.txtDepartamento.getText()+_view.txtEmail.getText() +_view.txtCelRecidencia.getText()+_view.txtTelTrabajo.getText() +_view.txtFechaCredito.getText() +_view.txtTipoCartera.getText() +_view.txtValorCredito.getText()+_view.txtNumCuotas.getText());//**************
         //Se puede usar una variable o el control, lo ideal es que se pudiera llamar una función de
        // validación aquí sobre los datos obteindos de las cajas de texto
        System.out.print (leyenda);
@@ -54,6 +54,13 @@ _view.txtProfesion.setText(null);
 _view.txtDireccion.setText(null);
 _view.txtCiudad.setText(null);
 _view.txtDepartamento.setText(null);
+_view.txtEmail.setText(null);
+_view.txtCelRecidencia.setText(null);
+_view.txtTelTrabajo.setText(null);
+_view.txtFechaCredito.setText(null);
+_view.txtTipoCartera.setText(null);
+_view.txtValorCredito.setText(null);
+_view.txtNumCuotas.setText(null);
 
 }
 }
